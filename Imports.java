4d6817697a3df26ba677211f2e9590eb5b34a78f
NickySf <72817817+NickySf@users.noreply.github.com>
2021-01-28 22:04:37 +0200

    Checkstyle

--- Imports.java
+++ Imports.java
@@ -4,128 +4,122 @@ in order for the user to insert data*/
 import java.util.Scanner;
 
 public class Imports {
-	Scanner input = new Scanner(System.in);
-	Converts obj = new Converts();
+  Scanner input = new Scanner(System.in);
+  Converts obj = new Converts();
 
-	// Test Result//
-	public String testResult() {
-		try {
-			int tr = Graphics.result();
-			while (tr != 1 && tr != 2) {
-				tr = Graphics.result();
-			}
-			String test_result = obj.convertResults(tr);
-			return test_result;
-		} catch (NumberFormatException e) {
-			System.out.println("\nInvalid Input");
-			System.out.println("Please try again:\n");
-			int tr = Graphics.result();
-			while (tr != 1 && tr != 2) {
-				tr = Graphics.result();
-			}
-			String test_result = obj.convertResults(tr);
-			return test_result;
-		}
+  // Test Result//
+  public String testResult() {
+    try {
+      int tr = Graphics.result();
+      while (tr != 1 && tr != 2) {
+        tr = Graphics.result();
+      }
+      String test_result = obj.convertResults(tr);
+      return test_result;
+    } catch (NumberFormatException e) {
+      System.out.println("\nInvalid Input");
+      System.out.println("Please try again:\n");
+      int tr = Graphics.result();
+      while (tr != 1 && tr != 2) {
+        tr = Graphics.result();
+      }
+      String test_result = obj.convertResults(tr);
+      return test_result;
+    }
+  }
 
-	}
+  // Name//
+  public String theName() {
+    String mess = "Please enter the name";
+    String name = Graphics.imports2(mess);
+    return name;
+  }
 
-	// Name//
-	public String theName() {
-		String mess = "Please enter the name";
-		String name = Graphics.imports2(mess);
-		return name;
-	}
+  // Surname//
+  public String theSurname() {
+    String mess = "Please enter the surname";
+    String surname = Graphics.imports2(mess);
+    return surname;
+  }
 
-	// Surname//
-	public String theSurname() {
-		String mess = "Please enter the surname";
-		String surname = Graphics.imports2(mess);
-		return surname;
-	}
+  // Age//
+  public int theAge() {
+    String mess = "Please enter the Age";
+    try {
+      int age = Graphics.imports1(mess);
+      return age;
+    } catch (NumberFormatException e) {
+      int age = Graphics.imports1(mess);
+      return age;
+    }
+  }
 
-	// Age//
-	public int theAge() {
-		String mess = "Please enter the Age";
-		;
-		try {
-			int age = Graphics.imports1(mess);
-			return age;
-		} catch (NumberFormatException e) {
-			int age = Graphics.imports1(mess);
-			return age;
-		}
-	}
+  // Gender//
+  public String theGender() {
+    int gender = Graphics.gender();
+    while (gender != 1 && gender != 2 && gender != 3) {
+      System.out.println("Wrong answer, please try again");
+      gender = Graphics.gender();
+    }
+    String g = obj.convertGender(gender);
+    return g;
+  }
 
-	// Gender//
-	public String theGender() {
+  // Date//
+  public String theDate() {
+    String mess = "Please enter the Date";
+    String date = Graphics.imports2(mess);
+    return date;
+  }
 
-		int gender = Graphics.gender();
-		while (gender != 1 && gender != 2 && gender != 3) {
-			System.out.println("Wrong answer, please try again");
-			gender = Graphics.gender();
-		}
-		String g = obj.convertGender(gender);
-		return g;
-	}
+  // Email//
+  public String theEmail() {
+    String mess = "Please enter the Email";
+    String email = Graphics.imports2(mess);
+    return email;
+  }
 
-	// Date//
-	public String theDate() {
-		String mess = "Please enter the Date";
-		String date = Graphics.imports2(mess);
-		return date;
-	}
+  // Phone//
+  public String thePhoneNumber() {
+    String mess = "Please enter the phone number";
+    String phone_number = Graphics.imports2(mess);
+    return phone_number;
+  }
 
-	// Email//
-	public String theEmail() {
-		String mess = "Please enter the Email";
-		String email = Graphics.imports2(mess);
-		return email;
-	}
+  // Area//
+  public String theArea() {
+    try {
+      int a = Graphics.menu();
+      while (a < 1 || a > 11) {
+        a = Graphics.menu();
+      }
+      String area = obj.convertArea(a);
+      return area;
+    } catch (NumberFormatException e) {
+      int a = Graphics.menu();
+      while (a < 1 || a > 11) {
+        a = Graphics.menu();
+      }
+      String area = obj.convertArea(a);
+      return area;
+    }
+  }
 
-	// Phone//
-	public String thePhoneNumber() {
-		String mess = "Please enter the phone number";
-		;
-		String phone_number = Graphics.imports2(mess);
-		return phone_number;
+  // Amka//
+  public String theAmka() {
+    String mess = "Please enter the AMKA";
+    String amka = Graphics.imports2(mess);
+    return amka;
+  }
 
-	}
-
-	// Area//
-	public String theArea() {
-		try {
-			int a = Graphics.menu();
-			while (a < 1 || a > 11) {
-				a = Graphics.menu();
-			}
-			String area = obj.convertArea(a);
-			return area;
-		} catch (NumberFormatException e) {
-			int a = Graphics.menu();
-			while (a < 1 || a > 11) {
-				a = Graphics.menu();
-			}
-			String area = obj.convertArea(a);
-			return area;
-
-		}
-	}
-
-	// Amka//
-	public String theAmka() {
-		String mess = "Please enter the AMKA";
-		String amka = Graphics.imports2(mess);
-		return amka;
-	}
-
-	// Statistics Information//
-	public int theStatistics() {
-		int reply = Graphics.theStatistics();
-		while (reply != 1 && reply != 2) {
-			System.out.println("Wrong answer, please try again");
-			reply = Graphics.theStatistics();
-		}
-		return reply;
-	}
+  // Statistics Information//
+  public int theStatistics() {
+    int reply = Graphics.theStatistics();
+    while (reply != 1 && reply != 2) {
+      System.out.println("Wrong answer, please try again");
+      reply = Graphics.theStatistics();
+    }
+    return reply;
+  }
 
 }
--- PotenciallyInfected.java
+++ PotenciallyInfected.java
@@ -1,50 +1,52 @@
 import java.util.ArrayList;
+
 public class PotenciallyInfected {
+  private String name;
+  private String surname;
+  private int age;
+  private String gender;
+  private String phone;
+  private String email;
+  private String amka;
+  private static int count = 0;
 
-	private String name;
-	private String surname;
-	private int age;
-	private String gender;
-	private String phone;
-	private String email;
-	private String amka;
-	private static int count= 0;
+  //Declaration of the list with the patients' contacts//
 
-    //Declaration of the list with the patients' contacts//
-	public static ArrayList <PotenciallyInfected> cases = new ArrayList <PotenciallyInfected>();
+  public static ArrayList<PotenciallyInfected> cases = new ArrayList<PotenciallyInfected>();
 
-	public PotenciallyInfected(String name, String surname,int age, String gender, String phone, String email, String amka) {
-		this.name = name;
-		this.surname = surname;
-		this.age = age;
-		this.gender = gender;
-		this.phone = phone;
-		this.email = email;
-		this.amka = amka;
-		count++;
-		cases.add(this);
-	}
+  public PotenciallyInfected(String name, String surname, int age, String gender,
+      String phone, String email, String amka) {
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    this.gender = gender;
+    this.phone = phone;
+    this.email = email;
+    this.amka = amka;
+    count++;
+    cases.add(this);
+  }
 
-    //Print of the potencially infected list//
-    public static void res() {
-		for (int i = 0; i<cases.size(); i++) {
-			System.out.println(cases.get(i));
-		}
-	}
+  //Print of the potencially infected list//
+  public static void res() {
+    for (int i = 0; i < cases.size(); i++) {
+      System.out.println(cases.get(i));
+    }
+  }
 
 
-	@Override
-	public String toString() {
-		return "PotenciallyInfected [name=" + name + ", surname=" + surname + ", age=" + age + ", gender=" + gender
-				+ ", phone=" + phone + ", email=" + email + ", amka=" + amka + "]";
-	}
+  @Override
+  public String toString() {
+    return "PotenciallyInfected [name=" + name + ", surname=" + surname + ", age=" + age
+        + ", gender=" + gender + ", phone=" + phone + ", email=" + email + ", amka=" + amka + "]";
+  }
 
-	//Search of the new patient in the potencially infected list and delete them if they exist//
-	public static void delete(String amka) {
-		int x = -1;
-		x = cases.indexOf(amka);
-		if (x != -1) {
-			cases.remove(x);
-		}
-	}
+  //Search of the new patient in the potencially infected list and delete them if they exist//
+  public static void delete(String amka) {
+    int x = -1;
+    x = cases.indexOf(amka);
+    if (x != -1) {
+      cases.remove(x);
+    }
+  }
 }
