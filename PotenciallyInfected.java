ece0ef75022a454f10fc4d0a7e776875b79ed0dd
NickySf <72817817+NickySf@users.noreply.github.com>
2021-01-29 14:38:34 +0200

    Javadoc

--- PotenciallyInfected.java
+++ PotenciallyInfected.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
-
+/** This class is about the
+ * people that came in contact
+ * with the patients
+ */
 public class PotenciallyInfected {
   private String name;
   private String surname;
@@ -10,10 +13,17 @@ public class PotenciallyInfected {
   private String amka;
   private static int count = 0;
 
-  //Declaration of the list with the patients' contacts//
-
   public static ArrayList<PotenciallyInfected> cases = new ArrayList<PotenciallyInfected>();
-
+    /** constructor for the
+     * object of the potencially infected
+     * @param name the contact's name
+     * @param surname the contact's surname
+     * @param age the contact's age
+     * @param gender the contact's gender
+     * @param phone the contact's phone
+     * @param email the contact's email
+     * @param amka the contact's amka
+     */
   public PotenciallyInfected(String name, String surname, int age, String gender,
       String phone, String email, String amka) {
     this.name = name;
@@ -26,8 +36,9 @@ public class PotenciallyInfected {
     count++;
     cases.add(this);
   }
-
-  //Print of the potencially infected list//
+    /**This method Prints
+     * the potencially infected list//
+     */
   public static void res() {
     for (int i = 0; i < cases.size(); i++) {
       System.out.println(cases.get(i));
@@ -36,12 +47,17 @@ public class PotenciallyInfected {
 
 
   @Override
+      /** Typical toString method
+     */
   public String toString() {
     return "PotenciallyInfected [name=" + name + ", surname=" + surname + ", age=" + age
         + ", gender=" + gender + ", phone=" + phone + ", email=" + email + ", amka=" + amka + "]";
   }
-
-  //Search of the new patient in the potencially infected list and delete them if they exist//
+      /**This method Searches the new patient
+       * in the potencially infected list
+       * and delete them if they exist//
+       * @param amka
+     */
   public static void delete(String amka) {
     int x = -1;
     x = cases.indexOf(amka);
